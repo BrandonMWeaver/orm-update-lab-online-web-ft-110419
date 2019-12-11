f3c3dd2aee3ecb10ee374373d85d27443dcef508
Brandon Weaver <a7xncob@gmail.com>
2019-12-11 19:54:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,7 +12,7 @@ class Student
   
   def self.create_table
     sql = <<-SQL
-      CREATE TABLE students (
+      CREATE TABLE IF NOT EXISTS students (
         id INTEGER PRIMARY KEY,
         name TEXT,
         grade INTEGER
@@ -22,4 +22,9 @@ class Student
     DB[:conn].execute(sql)
   end
   
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE students;
+    SQL
+  end
 end
